afc68d412200787cbefb2f3464c71a67d9543465
Divinegirl8 <iamoluchimercy@gmail.com>
2026-04-13 10:23:32 +0100

    step

---
+++ src/feature/VerifyEmail.tsx
@@ -0,0 +1,34 @@
+import Step from "../component/Step";
+import processImg from "../assets/onboarding/Group (10).png";
+
+interface StepConfig {
+  id: number;
+  label: string;
+}
+
+const VerifyEmail = () => {
+  const STEPS: StepConfig[] = [
+    { id: 1, label: 'Account setup' },
+    { id: 2, label: 'Verify email' },
+    { id: 3, label: 'Practice identity' },
+    { id: 4, label: 'Practice details' },
+    { id: 5, label: 'Compliance & terms' },
+  ];
+
+  return (
+    <div className="flex w-full h-screen justify-evenly">
+      
+   
+      <div className="">
+        <img src={processImg} className="w-full max-w-md" />
+      </div>
+
+      <div className="">
+        <Step steps={STEPS} currentStep={3} />
+      </div>
+
+    </div>
+  );
+};
+
+export default VerifyEmail;
