8ed3a170bfc3fb9e9d94e632f0428cd94a1a18f8
Abby-uye <princessabigail133@gmail.com>
2026-04-13 14:43:02 +0100

    feat/onboarding

--- src/feature/SignUp.tsx
+++ src/feature/SignUp.tsx
@@ -1,10 +1,23 @@
-const SignUp = () => {
-    return(
-        <div className="">
+import { useState } from "react";
+import Onboarding from "../component/Onboarding";
+import Step from "../component/Step";
 
-            <h1 className="text-2xl text-pink-400 text-center">Signup Upcoming</h1>
+const SignUp = () => {
+  const steps = [
+    { id: 1, label: "Account" },
+    { id: 2, label: "Profile" },
+    { id: 3, label: "Review" },
+  ];
+  const [currentStep, setCurrentStep] = useState(1);
+  return (
+    <div className="">
+      <Onboarding>
+        <div>
+          <Step steps={steps} currentStep={currentStep} />
         </div>
-    )
-}
+      </Onboarding>
+    </div>
+  );
+};
 
 export default SignUp;
